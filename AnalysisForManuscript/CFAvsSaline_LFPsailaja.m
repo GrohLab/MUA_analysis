7da7d07d9b1bca815fbce3909e5dc9c6f3a50886
Alexbmerkel <50135047+Alexbmerkel@users.noreply.github.com>
2020-02-08 15:05:43 +0100

    added file path

---
+++ AnalysisForManuscript/CFAvsSaline_LFPsailaja.m
@@ -0,0 +1,310 @@
+
+
+%analyze LFP to compare CFA and Saline
+
+
+clear all
+%cfa
+
+    
+% I am confused about the silicon probe recordings
+
+%Juxta
+
+  'Z:\Sailaja\Manuscript\Data\Juxtasomal\Saline\M104_C2\M104_C2_5mW_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\Saline\M104_C4\M104_C4_ Mech_5mW_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\Saline\M104_C5\M104_C5_Mech_5mW_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\Saline\M106_C1\M106_C1_Mech+5mW_edited_UMS_analysis.mat'...  
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\Saline\M106_C2\M106_C2_Mech+5mW_new_UMS_analysis.mat'...  
+    
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\CFA\M107_C2\M107_C2_Mech+5mW_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\CFA\M110_C4\M110_C4_Mech+6mW_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\CFA\M110_C5\M110_C5_Mech+6mW_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\CFA\M113_C4\M113_C4_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\CFA\M113_C6\M113_C6_Mech+L6_UMS_analysis.mat'...
+    'Z:\Sailaja\Manuscript\Data\Juxtasomal\CFA\M133_C2\M113_C2_Mech+L6alone edited_UMS_analysis.mat'...
+%16 channel saline
+    'Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\Saline\M7_C3\M7_C3_Mech_05mWanalysis.mat'
+%16 channel saline
+    'Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\CFA\VPL and L6\24_8_2018\M16_C2\M16_C2_Mech+L6_4mWanalysis.mat'
+
+   % 'Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\CFA\L6 alone\27_12_2018\M47_C2_L6Mech+L61mWanalysis.mat'}
+%     % 'Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\27_12_2018\M47_C2_L6Mech+L61mWanalysis.mat'...  %this is a repeat?
+% 
+%     Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\CFA\VPL and L6\09_11_2018\M3_C1   M3_C1_L6_Mechanalysis.mat
+%   %  Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\CFA\VPL and L6\09_11_2018\M3_C2  %no analysis!
+%   
+%   Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\CFA\VPL and L6\12_3_2019\M59_C1  M59_C1_HL +Terminal sti_1mWanalysis.mat
+%   Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\CFA\VPL and L6\12_3_2019\M59_C2\C2A M59_C2_HL +Terminal sti_1mWanalysis.mat
+%   Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\CFA\VPL and L6\12_3_2019\M59_C2\C2B M59_C2_HL +Terminal sti_2mWanalysis.mat
+%   
+%     'Z:\Sailaja\Manuscript\Data\16 channel\Poly2A probe\M7_C3\M7_C3_Mech_05mWanalysis.mat'...
+ %'Z:\Sailaja\Manuscript\Data\16 channel\standard 16 channel\probe\M137_C4\M137_C4_Mech+L6 05mWanalysis.mat'}
+
+%% ROSS FILES HERE
+
+%%
+
+for I=1:numel(files)
+    close all
+    clear params EEG spikeFindingData Conditions Triggers
+    load(files{I},'EEG','spikeFindingData','Conditions','Triggers')
+    lfp=EEG.data;
+    ppms=spikeFindingData.ppms;
+    fs=spikeFindingData.ppms*1000;
+    mech=Conditions{1}.Triggers;
+    %all triggers for spontaneous:
+    trigs=[];
+    for j=1:numel(Conditions);tr=Conditions{j}.Triggers; if size(tr,1)>size(tr,2), tr=tr';end;trigs=[trigs tr];end
+    
+    %lfp=zscore(EEG.data);
+    
+    
+%     %simple spectrogram: spontaneous
+%     %chronux parameters;
+%     Params.Fs=fs  %WE WILL DOWNSAMPLE
+%     params.fpass=[0 200]; % band of frequencies to be kept
+%     Ktapers=12;
+%     NW=(Ktapers+1)/2;
+%     params.tapers=[NW Ktapers]; % taper parameters
+%     params.pad=2; % pad factor for fft   
+
+    % get rid line noise (50 Hz)
+    %[f] = delineTest(lfp,fs)
+   % LFP = delineSignal(lfp,fs,(1:15)*f, 4);  %what is 1:9??? and 4???
+    LFP=lfp;
+    
+     
+    %stick signals together
+    cdata=[LFP';Triggers.whisker';Triggers.light'];
+    
+   
+    
+    %gather stim-triggered epochs===================================
+    timeBefore=-10.5;timeAfter=-.5;
+    X=TriggeredEpochs(cdata,trigs,fs*timeBefore, fs*timeAfter);
+    lfp_spont=squeeze(X(1,:,:));
+    lfp_spont=lfp_spont(:,~isnan(sum(lfp_spont))); %get rid of bad trials)
+    lfp_spont=resample(lfp_spont,1,round(ppms));%Downsample to 1kHz
+    
+    %mechanical stimulus
+    timeBefore=.5;timeAfter=2;
+    X_mech=TriggeredEpochs(cdata,mech,fs*timeBefore, fs*timeAfter);
+    lfp_mech=squeeze(X_mech(1,:,:));
+    lfp_mech=lfp_mech(:,~isnan(sum(lfp_mech))); %get rid of bad trials)
+    lfp_mech=resample(lfp_mech,1,round(ppms));%Downsample to 1kHz
+    mechstim=resample(squeeze(X_mech(2,:,:)),1,round(ppms));
+    
+    figure
+    plot(mechstim)  %check the stimulus
+    
+    
+  
+    %CHRONUX PARAMETERS
+    params.Fs=fs/ppms;  %WE DOWNSAMPLE af
+    params.fpass=[0 120]; % band of frequencies to be kept
+    Ktapers=12;
+    NW=(Ktapers+1)/2;
+    params.tapers=[NW Ktapers]; % taper parameters
+    params.pad=2; % pad factor for fft
+    params.err=[2 0.05];
+    params.trialave=1;
+    movingwin=[2 0.05];
+    segave=1;
+    winseg=1
+    
+    
+    % power spectra for spontaneous and mechanical
+    lfp_spont=rmlinesc(lfp_spont,params); %remove line noise for ds signal
+    lfp_mech=rmlinesc(lfp_mech,params); %remove line noise for ds signal
+    
+    figure   %plot it all
+    [S,f]=mtspectrumc(lfp_spont,params);
+    plot(f,10*log10(S)); xlabel('Frequency Hz'); ylabel('Spectrum');
+    spectra.Sspont=S; spectra.f=f;
+    hold on
+    [S,f]=mtspectrumc(lfp_mech,params);
+    plot(f,10*log10(S)); xlabel('Frequency Hz'); ylabel('Spectrum');
+    spectra.Smech=S;
+
+    %     figure;
+    %     [S,f, Serr]=mtspectrumc(lfp_trials,params);
+    %     plot(f,10*log10(S),f,10*log10(Serr(1,:)),f,10*log10(Serr(2,:))); xlabel('Frequency Hz'); ylabel('Spectrum');
+    %%% pause
+    
+    
+    %time resolved spectrogram for before and after stimulus trigger (t=0)
+    timeBefore=-5;timeAfter=12; 
+    condSpect=struct([]) 
+    for ii=1:numel(Conditions)
+        trigs=Conditions{ii}.Triggers;
+        trigs=trigs(find(trigs<(size(cdata,2)-timeAfter*fs)));
+        X=TriggeredEpochs(cdata,trigs,fs*timeBefore, fs*timeAfter);
+        lfp_trials=squeeze(X(1,:,:));
+        lfp_trials=lfp_trials(:,~isnan(sum(lfp_trials))); %get rid of bad trials)
+        lfp_trials=resample(lfp_trials,1,round(ppms));%Downsample to 1kHz
+        lfp_trials=rmlinesc(lfp_trials,params);%remove line noise for ds signal
+
+        stim=squeeze(X(2,:,:));
+        stim=stim(:,~isnan(sum(stim))); %get rid of bad trials)
+        stim=resample(stim,1,round(ppms));%Downsample to 1kHz
+        tt=[(timeBefore*fs):(timeAfter*fs)]/fs;
+        tt=resample(tt,1,round(ppms));%Downsample to 1kHz
+
+    
+        light=squeeze(X(3,:,:));
+        light=light(:,~isnan(sum(light))); %get rid of bad trials)
+        light=resample(light,1,round(ppms));%Downsample to 1kHz
+        tt=[(timeBefore*fs):(timeAfter*fs)]/fs;
+        tt=resample(tt,1,round(ppms));%Downsample to 1kHz
+        
+        [S,t,f]=mtspecgramc(lfp_trials,movingwin,params);
+        S=S';
+        condSpect(ii).tt=tt;
+        condSpect(ii).t=t;
+        condSpect(ii).f=f;
+        condSpect(ii).params=params;
+        condSpect(ii).S=S;
+        condSpect(ii).stim=mean(stim'/max(max(stim)));
+        condSpect(ii).light=mean(light'/max(max(light)))
+    end
+    
+
+    blwindow=(t<=-timeBefore);
+    baseline=mean(S(:,blwindow)');
+    dbS=10*log10((S./baseline'));
+    figure
+    pcolor(t+timeBefore,f',dbS);shading flat;
+    %pcolor(t+timeBefore,f',S);shading flat;
+
+    spectrogram.S=S; spectrogram.f=f;spectrogram.t=t;
+    spectrogram.params=params;
+  
+
+    colormap fire
+    hold on
+    plot(tt,mean(stim'/max(max(stim)))*10 - 11);
+    ylim([-10 120])
+    %plot(zeros(size(f)),f,'-k','linewidth',2)
+    [FILEPATH,NAME,EXT] = fileparts(files{I}); 
+    cd(FILEPATH)
+    save ChronuxResults 'spectrogram' 'spectra' Conditions condSpect NAME FILEPATH
+
+end
+
+
+
+%%      population analysis, load it all in
+Ss={};files=saline;
+for I=1:numel(saline)
+    [FILEPATH,NAME,EXT] = fileparts(files{I}); 
+    cd(FILEPATH)
+   Ss{I}=load('ChronuxResults')
+end
+
+
+Scfa={};files=cfa;
+for I=1:numel(cfa)
+    [FILEPATH,NAME,EXT] = fileparts(files{I}); 
+    cd(FILEPATH)
+   Scfa{I}=load('ChronuxResults')
+end
+
+
+%% figure  spontaneous power spectra
+figure
+subplot(2,2,2)
+S=cellfun(@(x) x.spectra,Scfa,'UniformOutput', false);
+Sspont=cell2mat(cellfun(@(x) x.Sspont,S,'UniformOutput', false));
+f=cellfun(@(x) x.spectra,Scfa,'UniformOutput', false);
+f=cell2mat(cellfun(@(x) x.f',f,'UniformOutput', false));
+
+plot(f,10*log10(Sspont),'linewidth',2); xlabel('Frequency Hz'); ylabel('Spectrum');
+legend(cfa)
+subplot(2,2,1)
+S=cellfun(@(x) x.spectra,Ss,'UniformOutput', false);
+Sspont=cell2mat(cellfun(@(x) x.Sspont,S,'UniformOutput', false));
+f=cellfun(@(x) x.spectra,Ss,'UniformOutput', false);
+f=cell2mat(cellfun(@(x) x.f',f,'UniformOutput', false));
+plot(f,10*log10(Sspont),'linewidth',2); xlabel('Frequency Hz'); ylabel('Spectrum');
+legend(saline)
+
+subplot(2,2,3)
+S=cellfun(@(x) x.spectra,Scfa,'UniformOutput', false);
+Sspont=cell2mat(cellfun(@(x) x.Sspont,S,'UniformOutput', false));
+f=cellfun(@(x) x.spectra,Scfa,'UniformOutput', false);
+f=cell2mat(cellfun(@(x) x.f',f,'UniformOutput', false));
+plot(f,10*log10(Sspont),'r','linewidth',2); xlabel('Frequency Hz'); ylabel('Spectrum');
+S=cellfun(@(x) x.spectra,Ss,'UniformOutput', false);
+Sspont=cell2mat(cellfun(@(x) x.Sspont,S,'UniformOutput', false));
+f=cellfun(@(x) x.spectra,Ss,'UniformOutput', false);
+f=cell2mat(cellfun(@(x) x.f',f,'UniformOutput', false));
+hold on
+plot(f,10*log10(Sspont),'k'); xlabel('Frequency Hz'); ylabel('Spectrum');
+
+
+
+%% loop to save bispikes
+
+
+%%  this 
+
+
+
+
+
+
+%% SCRAP
+LFP=resample(double(lfp),1,round(ppms)); LFP=LFP(1:400000);
+[imf,residual,info] = emd(LFP,'Interpolation','pchip');
+[hs,f,t,imfinsf,imfinse]=hht(imf,fs/round(ppms),'FrequencyLimits',[0 200]);
+
+%% HHT analysis  does not work for short recordings?
+figure
+ax=[];
+ax(1)=subplot(3,1,2)
+hht(imf,fs,'FrequencyLimits',[0 24]);
+colormap fire
+cmap=colormap;
+colormap(flipud(cmap))
+%%colormap(flipud(cb))
+%brighten(.5)
+%set(gca,'color',cmap(1,:))
+set(gca,'color',[1 1 1]*.7)
+
+%%
+
+ax(2)=subplot(3,1,3)
+% hht(imf,fs,'FrequencyLimits',[25 150],  'FrequencyResolution',1);
+% colormap fire
+% set(gca,'color',cmap(1,:))
+
+
+[imf,residual,info] = emd(LFP,'Interpolation','pchip', 'MaxNumIMF',20);
+[hs,f,t,imfinsf,imfinse]=hht(imf,fs/round(ppms),'FrequencyLimits',[0 200]);
+goods=find(max(imfinsf)>3);
+new_lfp=sum(imf(:,goods)');
+figure
+plot(LFP)
+hold on;plot(new_lfp)
+
+%% HHT analysis  does not work for short recordings?
+figure
+ax=[];
+ax(1)=subplot(3,1,2)
+hht(imf,fs,'FrequencyLimits',[0 24]);
+colormap fire
+cmap=colormap;
+colormap(flipud(cmap))
+%%colormap(flipud(cb))
+%brighten(.5)
+%set(gca,'color',cmap(1,:))
+set(gca,'color',[1 1 1]*.7)
+
+%%
+
+ax(2)=subplot(3,1,3)
+
+
+
+
